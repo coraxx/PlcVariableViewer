8fd0ecfc5208040312630cf06bd5ecd40f128975
Jan Arnold <jan.arnold@coraxx.net>
2018-07-25 17:19:55 +0200

    - cleanup

--- TwinCatVariableViewer/TwinCatVariableViewer/MainWindow.xaml.cs
+++ TwinCatVariableViewer/TwinCatVariableViewer/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Threading;
 using TwinCAT;
